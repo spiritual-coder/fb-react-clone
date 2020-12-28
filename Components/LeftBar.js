b32a36bae3fd4a9fd8c5afae6b19f16819bb8c94
Gaurav Singh <38595651+spiritual-coder@users.noreply.github.com>
2020-12-28 11:47:24 +0530

    Update LeftBar.js

--- Components/LeftBar.js
+++ Components/LeftBar.js
@@ -1,9 +1,50 @@
-import React from "react";
+import React, { Component } from "react";
+import LeftNavList from "./LeftNavList";
+import SidenavItems from "./SidenavItems.js";
 
-export default function LeftBar() {
-  return (
-    <>
-      <div className="leftSidebar">Left SideBar</div>
-    </>
-  );
+class LeftBar extends Component {
+  state = {
+    itemList: [],
+  };
+
+  getItems = () => {
+    LeftNavList().then((itemName) => {
+      this.setState({
+        itemList: itemName,
+      });
+    });
+  };
+
+  componentDidMount() {
+    this.getItems();
+  }
+
+  render() {
+    return (
+      <>
+        <div className="leftSidebar">
+          <div className="navigationList">
+            <div>
+              {this.state.itemList.map(({ itemName, itemLink, itemId }) => (
+                <SidenavItems
+                  itemName={itemName}
+                  itemLink={itemLink}
+                  key={itemId}
+                />
+              ))}
+            </div>
+          </div>
+          <div className="shortcuts">
+            <header>
+              <div className="headerTitle">
+                <h2>Shortcuts</h2>
+              </div>
+            </header>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
+
+export default LeftBar;
