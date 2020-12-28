a48b1500f74e4a4a278945f3ef62812e88ee049c
Gaurav Singh <38595651+spiritual-coder@users.noreply.github.com>
2020-12-28 11:45:51 +0530

    Code Update

--- Components/RightBar.js
+++ Components/RightBar.js
@@ -1,9 +1,58 @@
-import React from "react";
+import React, { Component } from "react";
+import ContactsList from "./ContactsList";
+import OnlineContacts from "./OnlineContacts.js";
 
-export default function RightBar() {
-  return (
-    <>
-      <div className="rightSidebar"> Right SideBar</div>
-    </>
-  );
+class RightBar extends Component {
+  state = {
+    onlineList: [],
+  };
+
+  getProfiles = () => {
+    ContactsList().then((contactsName) => {
+      this.setState({
+        onlineList: contactsName,
+      });
+    });
+  };
+
+  componentDidMount() {
+    this.getProfiles();
+  }
+
+  render() {
+    return (
+      <>
+        <div className="rightSidebar">
+          <div className="notifications">
+            <header>
+              <div className="headerTitle">
+                <h2>Birthdays</h2>
+              </div>
+            </header>
+          </div>
+          <div className="contacts">
+            <header>
+              <div className="headerTitle">
+                <h2>Contacts</h2>
+              </div>
+              <div className="headerIcons"></div>
+            </header>
+            <div>
+              {this.state.onlineList.map(
+                ({ contactsName, contactsImage, personId }) => (
+                  <OnlineContacts
+                    contactsName={contactsName}
+                    contactsImage={contactsImage}
+                    key={personId}
+                  />
+                )
+              )}
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
+
+export default RightBar;
