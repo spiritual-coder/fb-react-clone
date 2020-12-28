77b9556aa014bd30fc4123264870a3d338d06191
Gaurav Singh <38595651+spiritual-coder@users.noreply.github.com>
2020-12-28 11:48:04 +0530

    Code Update

--- Components/Center.js
+++ Components/Center.js
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default function Center() {
-  return (
-    <>
-      <div className="centerContent">Main Center</div>
-    </>
-  );
+class Center extends Component {
+  render() {
+    return (
+      <>
+        <div className="centerContent">
+          <div className="addPost">
+            <form>
+              <input type="text" required />
+              <button type="submit">Post</button>
+            </form>
+          </div>
+
+          <div className="postCard">
+            <header className="postHead">
+              <div className="profilePic">
+                <img
+                  alt="profilepic"
+                  src="https://raw.githubusercontent.com/spiritual-coder/fb-react-clone/main/assets/images/1.png"
+                />
+              </div>
+              <div className="profileName">
+                <h3>Gaurav S Kaintura</h3>
+              </div>
+            </header>
+            <div className="postContent">
+              <p>lorem Ipsum Do re La Ti</p>
+            </div>
+            <div className="postImg"></div>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
+
+export default Center;
