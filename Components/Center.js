752b651f697eb9302eab862a8db242f43a02fc47
Gaurav Singh <38595651+spiritual-coder@users.noreply.github.com>
2020-12-30 13:18:34 +0530

    Code Update 4.0

--- Components/Center.js
+++ Components/Center.js
@@ -1,34 +1,41 @@
 import React, { Component } from "react";
+import Form from "./Form.js";
+import StoryList from "./StoryList.js";
+import PostList from "./PostList.js";
 
 class Center extends Component {
+  state = {
+    data: [
+      { topost: "I am learning Data Structures." },
+      { topost: "My First Post" },
+    ],
+  };
+
+  handleSubmit = (newVal) => {
+    this.setState({ data: [...this.state.data, newVal] });
+  };
+
+  //localStorage
+
+  componentDidUpdate() {
+    localStorage.setItem("dataStore", JSON.stringify(this.state.data));
+  }
+
+  componentDidMount() {
+    const dataStore = JSON.parse(localStorage.getItem("dataStore"));
+    if (dataStore !== null) {
+      this.setState({ data: dataStore });
+    }
+  }
+
   render() {
+    const { data } = this.state;
     return (
       <>
         <div className="centerContent">
-          <div className="addPost">
-            <form>
-              <input type="text" required />
-              <button type="submit">Post</button>
-            </form>
-          </div>
-
-          <div className="postCard">
-            <header className="postHead">
-              <div className="profilePic">
-                <img
-                  alt="profilepic"
-                  src="https://raw.githubusercontent.com/spiritual-coder/fb-react-clone/main/assets/images/1.png"
-                />
-              </div>
-              <div className="profileName">
-                <h3>Gaurav S Kaintura</h3>
-              </div>
-            </header>
-            <div className="postContent">
-              <p>lorem Ipsum Do re La Ti</p>
-            </div>
-            <div className="postImg"></div>
-          </div>
+          <StoryList />
+          <Form onSubmit={this.handleSubmit} />
+          <PostList topost={data} />
         </div>
       </>
     );
