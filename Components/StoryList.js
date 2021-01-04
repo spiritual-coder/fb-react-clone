f60d0f790674283541981f8b24041e64908489b0
Gaurav Singh <38595651+spiritual-coder@users.noreply.github.com>
2021-01-04 23:54:06 +0530

    4.5

--- Components/StoryList.js
+++ Components/StoryList.js
@@ -1,69 +1,32 @@
 import React, { Component } from "react";
+import StoryItem from "./StoryItem.js";
 
 class StoryList extends Component {
+  // state = {
+  //   story: [
+  //     {
+  //       toadd:
+  //         "https://images.pexels.com/photos/5117800/pexels-photo-5117800.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  //     },
+  //     {
+  //       toadd:
+  //         "https://images.pexels.com/photos/5627494/pexels-photo-5627494.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  //     },
+  //     {
+  //       toadd:
+  //         "https://images.pexels.com/photos/4131522/pexels-photo-4131522.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  //     },
+  //   ],
+  // };
   render() {
+    // const { story } = this.state;
+    const { toadd } = this.props;
+
     return (
       <div className="storyList">
-        <div className="storyCard">
-          <div className="storyContent">
-            {/* <div className="storyProfile">
-              <img
-                alt="profile."
-                src="https://randomuser.me/api/portraits/women/39.jpg"
-              />
-            </div> */}
-            <div className="storyImg">
-              <img
-                alt="story"
-                src="https://images.pexels.com/photos/5117800/pexels-photo-5117800.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-              />
-            </div>
-          </div>
-        </div>
-
-        <div className="storyCard">
-          <div className="storyContent">
-            <div className="storyImg">
-              <img
-                alt="story"
-                src="https://images.pexels.com/photos/5627494/pexels-photo-5627494.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-              />
-            </div>
-          </div>
-        </div>
-
-        <div className="storyCard">
-          <div className="storyContent">
-            <div className="storyImg">
-              <img
-                alt="story"
-                src="https://images.pexels.com/photos/4131522/pexels-photo-4131522.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-              />
-            </div>
-          </div>
-        </div>
-
-        <div className="storyCard">
-          <div className="storyContent">
-            <div className="storyImg">
-              <img
-                alt="story"
-                src="https://images.pexels.com/photos/3576433/pexels-photo-3576433.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-              />
-            </div>
-          </div>
-        </div>
-
-        <div className="storyCard">
-          <div className="storyContent">
-            <div className="storyImg">
-              <img
-                alt="story"
-                src="https://images.pexels.com/photos/5478104/pexels-photo-5478104.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-              />
-            </div>
-          </div>
-        </div>
+        {toadd.map((card, index) => {
+          return <StoryItem card={card.toadd} key={index} />;
+        })}
       </div>
     );
   }
